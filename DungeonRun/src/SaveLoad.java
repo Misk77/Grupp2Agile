bdc5da9d5a47094130ae6cc11580dab84e30d4ee
Michel <42797392+Misk77@users.noreply.github.com>
2019-01-11 11:40:11 +0100

    Update: GameMenuMain

--- DungeonRun/src/SaveLoad.java
+++ DungeonRun/src/SaveLoad.java
@@ -1,10 +1,30 @@
+import java.io.File;
 
-public class SaveLoad {
+public class SaveLoad<GameMenuMain> {
 	
 	public void saveToDisk() {//need to add the proper arguments, like name and treasure
+		
 	
-	}
+	}//saveTODisk
 	public void LoadFromDisk() {
 		
-	}
+	}//LoadFromDisk
+	
+	
+	
 }
+
+
+
+/*
+• Scanner har en konstruktor som tar ett Fileobjekt som parameter
+• Alternativt kan man koppla ihop den med
+en Reader-klass
+• File f = new File(“minfil.txt”);
+• Scanner in = new Scanner(f);
+//Kastar ett kontrollerat undantag
+• Scanner in2 = new Scanner(new
+FileReader(“minfil.txt”));
+• String str = in2.next();
+• in2.close();
+*/
