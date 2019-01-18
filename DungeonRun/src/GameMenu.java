fe0ecad61d4e2bc42829dfa785a40e39cb2d312b
Michel <42797392+Misk77@users.noreply.github.com>
2019-01-18 14:39:39 +0100

    UPDATE. Lekt med GameMenuFirst printen,

--- DungeonRun/src/GameMenu.java
+++ DungeonRun/src/GameMenu.java
@@ -185,8 +185,12 @@ public class GameMenu {
 //Games start here, then NEW GAME the follow the methods one by one tha nfinally into  Game class and the game is set to go running
 	public Object[] GameMenuFirst() {
 		GuiConsole.io.println();
-		GuiConsole.io.println("\nWelcome to the Dungeon Run!\n",Color.YELLOW);
-		GuiConsole.io.println("============================== Load or start a new game ===================================",Color.RED);
+		GuiConsole.io.println("|=======================================================================================================|",Color.RED);
+		GuiConsole.io.println("|================================== ||----------| DUNGEON RUN |----------|| =======================================|",Color.GREEN);
+		GuiConsole.io.println("|=======================================================================================================|",Color.YELLOW);
+		GuiConsole.io.println();
+		GuiConsole.io.println("======================================== Load or start a new game ========================================||",Color.WHITE);
+		GuiConsole.io.println();
 		GuiConsole.io.println("[L]OAD GAME \n",Color.MAGENTA);
 		GuiConsole.io.println("[N]EW GAME \n",Color.BLUE);
 		//System.out.println("[A]i Character \n");
