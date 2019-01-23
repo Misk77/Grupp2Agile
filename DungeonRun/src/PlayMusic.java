ceec975820b430e901625b6112e5b631269810ce
Michel <42797392+Misk77@users.noreply.github.com>
2019-01-23 13:01:20 +0100

    UPDATE: clip.loop(Clip.LOOP_CONTINUOUSLY); istället för att den ska starta om efter 10 gånger, tror inte det gör så mycket men då går den iaf hela tiden och behöver inte ladda om filen efter 10 gånger..  Ska kolla lite mera ang detta.... man kan ta tiden så pausar den streamen så länge tiden på klippet är ..kanske f¨r det att fungera

--- DungeonRun/src/PlayMusic.java
+++ DungeonRun/src/PlayMusic.java
@@ -4,101 +4,89 @@ import javax.sound.sampled.Clip;
 import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineUnavailableException;
 
-public class PlayMusic implements Runnable
-{
-    private boolean running = false;
-    private Thread thread;
+public class PlayMusic implements Runnable {
+	private boolean running = false;
+	private Thread thread;
 // make an instance of this there you wanna play then call the playbackground with a string path to the file you wanna play
-    
-    public PlayMusic()
-    {
-        start();
-    }
 
-    public void start()
-    {
-        if(running)
-            return;
-        this.thread = new Thread(this);
-       this. running = true;
-        this.thread.start();
-    }
+	public PlayMusic() {
+		start();
+	}
 
-    //
-    private boolean playSong = false;
-    private AudioInputStream inputStream;
-    private String url;
-    private Clip clip;
+	public void start() {
+		if (running)
+			return;
+		this.thread = new Thread(this);
+		this.running = true;
+		this.thread.start();
+	}
 
-    @Override
-    public void run()
-    {
-        while(running)
-        {
-            if(inputStream == null && playSong)
-            {
-                playSong = false;
-                try
-                {
-                	this. inputStream = AudioSystem.getAudioInputStream(PlayMusic.class.getResource(url));
-                	this. clip.open(inputStream);
-                	 FloatControl gainControl = 
-                	 		    (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-                	 		gainControl.setValue(0.0f); 
-                	this. clip.loop(10);
-                }
-                catch(Exception e)
-                {
-                	GuiConsole.io.println("Somthings wrong in run getting the AudioStream"+e);
-                }
-            }
-        }
-    }
+	//
+	private boolean playSong = false;
+	private AudioInputStream inputStream;
+	private String url;
+	private Clip clip;
 
-    public void playBackGround(String string) // call to play .wav file String is the path to the wav file and then add .wav from url variable
-    {
-        if(clip != null)
-        {
-        	this.clip.stop();
-        	this.clip.close();
-        }
-        try
-        {
-            clip = AudioSystem.getClip();
-        }
-        catch(LineUnavailableException e)
-        {
-        	GuiConsole.io.println("Somthings wrong in playBackGround getting the  AudioSystem.getClip"+e);
-        }
-        url = string + ".wav";
-        this.  playSong = true;
-        this. inputStream = null;
-    }
+	@Override
+	public void run() {
+		while (running) {
 
-    public void disposeSound()
-    {
-        if(clip != null)
-        {
-        	this.clip.loop(-1);
-        	this. clip.stop();
-        	this. clip.close();
-        }
-        this. clip = null;
-        this. playSong = false;
-        this. inputStream = null;
-    }
-    public void stopbackground()
-    {
-    	
-    	  this.clip.stop();
-          this.clip.close();
-         
-        
-       
-    }
- public void   changeVolume(int i){
-	 FloatControl gainControl = 
- 		    (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
- 		gainControl.setValue(-10.0f); 
-    }
-}//END CLASS
+			if (inputStream == null && playSong) {
+				playSong = false;
+				try {
+					this.inputStream = AudioSystem.getAudioInputStream(PlayMusic.class.getResource(url));
+					this.clip.open(inputStream);
+					FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+					gainControl.setValue(0.0f);
+					clip.loop(Clip.LOOP_CONTINUOUSLY); // change from clip.loop(10); to LOOP_CONTINUOUSLY
+				} catch (Exception e) {
+					GuiConsole.io.println("Somthings wrong in run getting the AudioStream" + e);
+				}
+			}
+		}
+	}
+
+	public void playBackGround(String string) // call to play .wav file String is the path to the wav file and then add
+												// .wav from url variable
+	{
+		if (clip != null) {
+			this.clip.stop();
+			this.clip.close();
+		}
+		try {
+			clip = AudioSystem.getClip();
+		} catch (LineUnavailableException e) {
+			GuiConsole.io.println("Somthings wrong in playBackGround getting the  AudioSystem.getClip" + e);
+		}
+		url = string + ".wav";
+		this.playSong = true;
+		this.inputStream = null;
+
+	}
+
+	public void disposeSound() {
+		if (clip != null) {
+			this.clip.loop(-1);
+			this.clip.stop();
+			this.clip.close();
+		}
+		this.clip = null;
+		this.playSong = false;
+		this.inputStream = null;
+	}
+
+	public void stopbackground() {
+
+		this.clip.stop();
+		this.clip.close();
+
+	}
+
+	public void changeVolume(int i) {
+		FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+		gainControl.setValue(-10.0f);
+	}
+
+	//////
+
+}// END CLASS
